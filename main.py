09fa44a7dd87e81ad8de5c2357d9aeb940104ee5
Matthias Osswald <matthias@bespinian.io>
2024-10-30 00:18:59 +0100

    Add login.

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
-from compendium.embeddings import get_documents
-from compendium.embeddings import load_embeddings
+from fastapi import FastAPI, Request, Form, Depends
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.responses import RedirectResponse
+import os
+
+from compendium.embeddings import get_documents, load_embeddings
 from compendium.predict import suggest_medication
 from compendium.split_from_all import get_medications
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.add_middleware(
+    SessionMiddleware, secret_key="ZHx579kzsILbuvhi")
+
+templates = Jinja2Templates(directory="templates")
+
+USERNAME = "test"
+PASSWORD = os.getenv("PASSWORD", "test")
 
 medications = get_medications()
 documents = get_documents(medications)
 vectorstore = load_embeddings()
 
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+def get_current_user(request: Request):
+    if not request.session.get("user"):
+        return RedirectResponse(url="/login")
+    return request.session["user"]
+
+@app.get("/login", response_class=HTMLResponse)
+async def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post("/login", response_class=HTMLResponse)
+async def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    if username == USERNAME and password == PASSWORD:
+        request.session["user"] = username
+        return RedirectResponse(url="/", status_code=302)
+    return templates.TemplateResponse("login.html", {"request": request, "error": "Invalid credentials"})
 
 @app.get("/", response_class=HTMLResponse)
-async def get_form(request: Request):
+async def get_form(request: Request, user=Depends(get_current_user)):
+    if isinstance(user, RedirectResponse):
+        return user
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/suggest", response_class=HTMLResponse)
-async def suggest(request: Request, symptoms: str = Form(...), age: int = Form(...), sex: str = Form(...)):
+async def suggest(request: Request, symptoms: str = Form(...), age: int = Form(...), sex: str = Form(...), user=Depends(get_current_user)):
+    if isinstance(user, RedirectResponse):
+        return user
     result = suggest_medication(
         symptoms,
         vectorstore,
